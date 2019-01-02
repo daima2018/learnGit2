9bc6ca461e4168b04d8d683501261fc8f7732d68
lvxianglong <lvxianglonggg@gmail.com>
2019-01-02 20:56:42 +0800

    111

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @description: hello world
+ * @author: lxl
+ * @create: 2019-01-02 20:54
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("lxl hello world");
+    }
+}
